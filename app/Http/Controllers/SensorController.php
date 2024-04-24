c61a945a6b519e96ae4f265cba92fe2e67294355
Angel T <angel.t069@outlook.com>
2024-04-23 22:12:57 -0600

    alarma

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -68,15 +68,10 @@ class SensorController extends Controller
         return response()->json(['msg' => 'Alarma apagada en todas las habitaciones'], 200);
     }
 
-<<<<<<< HEAD
     public function estadoAlarma() { 
         
         $habitacionesConAlarma = Habitacion::where('alarma', true)->pluck('id');
         $hayAlarmaDesactivada = Habitacion::where('alarma', false)->exists();
-=======
-    public function estadoAlarma() {
-        $habitaciones = Habitacion::where('alarma', true)->pluck('id');
->>>>>>> b1d0b5290668233bdb67ec901ed21b56ce1bbe99
     
         if ($habitaciones->isEmpty()) {
             return response()->json(['status' => false], 200);
