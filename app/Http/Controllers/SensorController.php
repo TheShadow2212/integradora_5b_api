3ae9961e7f669864c2dfadb808f384da5381961c
Alejandro <22170166@uttcampus.edu.mx>
2024-04-23 21:27:41 -0600

    Agregado

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -13,13 +13,18 @@ class SensorController extends Controller
 
     public function create(Request $request)
     {
-        $sensor = new Sensor();
-        $sensor->name = $request->name;
-        $sensor->data = $request->data;
-        $sensor->room_id = intval($request->room_id);
-        $sensor->date_time = \Carbon\Carbon::now()->toDateTimeString();
-        $sensor->save();
-        return response()->json(['msg' => 'Registrado correctamente', 'sensor' => $sensor], 200);
+        $rooms = Room::all();
+    
+        foreach ($rooms as $room) {
+            $sensor = new Sensor();
+            $sensor->name = $request->name;
+            $sensor->data = $request->data;
+            $sensor->room_id = $room->id;
+            $sensor->date_time = \Carbon\Carbon::now()->toDateTimeString();
+            $sensor->save();
+        }
+    
+        return response()->json(['msg' => 'Registrado correctamente en todas las habitaciones'], 200);
     }
 
 
